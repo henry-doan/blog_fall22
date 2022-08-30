f3b32df706b201c1fd50899fc9688239db675992
Henry Doan <henrydoan96@gmail.com>
2022-08-29 19:46:41 -0600

    page model, route controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # get '/pages', to: 'pages#index'
+  # post '/pages', to: 'pages#create'
+  namespace :api do
+    resources :pages
+  end
+
 end
